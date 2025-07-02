a13a0a12aaa84ef2bb8f9366354ce17387fbca1c
raccoon <raccoon@raccoon.fun>
2025-07-02 01:05:12 -0400

    monospace the pacemaker text

--- BGAnimations/ScreenGameplay underlay/PerPlayer/TargetScore/Pacemaker.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/TargetScore/Pacemaker.lua
@@ -2,7 +2,7 @@ local player, pss, isTwoPlayers, graph, target_score = unpack(...)
 local pn = ToEnumShortString(player)
 
 local pacemaker = Def.BitmapText{
-	Font="Common Bold",
+	Font="Wendy/_wendy monosmall numbers",
 	JudgmentMessageCommand=function(self)
 		self:queuecommand("Update")
 	end,
